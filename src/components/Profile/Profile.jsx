1b8b1d9d1e8b4788cbe6f6ee6295bd109a3b3614
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-06-13 20:11:14 -0600

    update 1 task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import { ProfileCont} from './Profile.styled';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <ProfileCont>
+      <div class="description">
+        <img src={avatar} alt={username} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </ProfileCont>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default Profile;
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const ProfileCont = styled.div`
+  width: 350px;
+  margin: 0 auto;
+  padding: 40px 20px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.8);
+`;
